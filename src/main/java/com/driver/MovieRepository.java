048227963529b72f0a7318a4d45368274cd8033f
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-08-31 11:48:10 +0530

    Watchlist done

---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,84 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+
+
+    HashMap<String , Movie> mdb = new HashMap<>();
+    HashMap<String , Director> ddb = new HashMap<>();
+
+    HashMap<String , List<String>> mddb = new HashMap<>();
+
+    public String addMovie(Movie movie) {
+        mdb.put(movie.getName() , movie);
+        return "Movie added success";
+    }
+
+    public void adddirector(Director director) {
+        ddb.put(director.getName() , director);
+    }
+
+    public void addMoviedirectorPair(String mname, String dname) {
+
+        if(mddb.containsKey(dname)) {
+            List<String> res = mddb.get(dname);
+            res.add(mname);
+            mddb.put(dname, res);
+        }
+        else {
+            List<String> newlist = new ArrayList<>();
+            newlist.add(mname);
+            mddb.put(dname,newlist);
+        }
+    }
+    public Movie getMovieByName(String mname) {
+
+        return mdb.get(mname);
+    }
+
+    public Director getDirectorByName(String dname) {
+        return ddb.get(dname);
+    }
+
+
+    public List<String> getMoviesByDirectorName(String dname) {
+
+        return mddb.get(dname);
+    }
+
+    public List<Movie> findAllMovies() {
+
+       List<Movie> list =  new ArrayList<>(mdb.values());
+       return list;
+    }
+
+    public void deleteDirectorByName(String dname) {
+
+        ddb.remove(dname);
+    }
+
+    public void deleteAllDirectors() {
+
+        List<String> dnames = new ArrayList<>();
+        List<List<String>> mnames = new ArrayList<>();
+        for(String dname : ddb.keySet())
+        {
+            ddb.remove(dname);
+            mnames.add(mddb.get(dname));
+            mddb.remove(dname);
+        }
+        for(List<String> movies : mnames)
+        {
+            for(String name : movies)
+            {
+                mdb.remove(name);
+            }
+        }
+    }
+}
